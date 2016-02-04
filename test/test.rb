8e4bc2979d4ac9480ae164b75632779cc56ca60e
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-03 21:21:47 -0500

    update tests

--- test/test.rb
+++ test/test.rb
@@ -5,28 +5,38 @@ require_relative "../frame"
 class TestBowlingGame < Test::Unit::TestCase
 
   def setup
-    @game = BowlingGame.new
   end
 
   def test_game_exists
-    assert(@game != nil)
+    game = BowlingGame.new
+    assert(game != nil, "Game doesn't exist.")
   end
 
   def test_game_has_ten_frames
     game = BowlingGame.new
-    assert(game.frames.length == 10)
+    assert(game.frames.length == 10, "Game doesn't have 10 frames.")
   end
 
-  def frame_only_allows_ten_points
+  def test_frame_only_allows_ten_points
+    frame = Frame.new
+    frame.bowl
+    assert(frame.score < 10, "Frame allows more than ten points")
   end
 
-  def frame_only_allows_two_rolls
+  def test_frame_only_allows_two_rolls
+    frame = Frame.new
+    frame.bowl
+    frame.bowl
+    assert(frame.bowl == Exception, "Frame allows more than two rolls")
   end
 
   def test_frame_spare_return_spare
   end
 
   def test_frame_strike_returns_strike
+  end
+
+  def teardown
   end
 
 end
