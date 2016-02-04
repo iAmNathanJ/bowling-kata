1505502f6aab83bc182e686f53109ca23844635a
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-04 08:55:18 -0500

    add more tests

--- test/test.rb
+++ test/test.rb
@@ -17,23 +17,41 @@ class TestBowlingGame < Test::Unit::TestCase
     assert(game.frames.length == 10, "Game doesn't have 10 frames.")
   end
 
-  def test_frame_only_allows_ten_points
+  def test_frame_allows_ten_pin_max
     frame = Frame.new
-    frame.bowl
-    assert(frame.score < 10, "Frame allows more than ten points")
+    frame.bowl(11)
+    assert(frame.pins == 10, "Frame allows more than ten points")
   end
 
-  def test_frame_only_allows_two_rolls
+  def test_frame_allows_two_roll_max
     frame = Frame.new
-    frame.bowl
-    frame.bowl
-    assert(frame.bowl == Exception, "Frame allows more than two rolls")
+    frame.bowl(1)
+    frame.bowl(1)
+    assert_raise do
+      frame.bowl
+    end
+  end
+
+  def test_frame_tracks_total_pins
+    frame = Frame.new
+    frame.bowl(1)
+    frame.bowl(1)
+    assert(frame.pins == 2)
   end
 
   def test_frame_spare_return_spare
+    game = BowlingGame.new
+    frame = game.frames[0]
+    frame.bowl(9)
+    frame.bowl(1)
+    assert(game.score_frame(frame) == "/")
   end
 
   def test_frame_strike_returns_strike
+    game = BowlingGame.new
+    frame = game.frames[0]
+    frame.bowl(10)
+    assert(game.score_frame(frame) == "x")
   end
 
   def teardown
