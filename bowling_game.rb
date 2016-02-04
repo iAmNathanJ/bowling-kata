cba53abe6f8c262bedc18b37414b2953c73f8141
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-03 21:22:08 -0500

    update bowling_game and frame

--- bowling_game.rb
+++ bowling_game.rb
@@ -2,13 +2,54 @@ require_relative "frame"
 
 class BowlingGame
 
-  attr_reader :frames
+  attr_accessor :frames, :current_frame
 
   def initialize
-    @frames = []
-    for i in (1..10)
-      @frames << Frame.new
+    @frames = build_frames
+    @current_frame = 0
+  end
+
+  def next_frame
+    @current_frame += 1
+  end
+
+  def score_frame(frame)
+    if frame.pins == 10
+      frame.rolls.length == 1 ? 'x' : '/'
+    else
+      frame.rolls
+    end
+  end
+
+  def auto_play
+    @frames.each do |frame|
+      begin
+        2.times do
+          frame.bowl unless frame.closed?
+        end
+        next_frame
+        p score_frame(frame)
+      rescue => e
+        e.message
+      end
+    end
+  end
+
+  def score_game
+    @frames.inject(0) do |score, frame|
+      score + score_frame(frame)
+    end
+  end
+
+  private
+
+  def build_frames
+    (1..10).inject([]) do |frames|
+      frames << Frame.new
     end
   end
 
 end
+
+game = BowlingGame.new
+game.auto_play
--- frame.rb
+++ frame.rb
@@ -1,29 +1,22 @@
 class Frame
 
-  attr_reader :score, :rolls, :pins, :complete
+  attr_reader :rolls
 
   def initialize
-    @score = 0
-    @rolls = 0
-    @pins = 0
-    @complete = false
+    @rolls = []
   end
 
-  def bowl
-
-    raise "You're out of rolls, my friend." if @rolls >= 2
-
-    @rolls += 1
-    @pins += (rand * (10-@pins)).round
-
-    if @pins == 10 || @rolls == 2
-      @complete = true
-    end
-
+  def bowl(p = (rand * (10-pins)).round)
+    raise "This frame is closed." if closed?
+    @rolls << p
   end
 
-  def get_score(extra_1 = 0, extra_2 = 0)
-    pins + extra_1 + extra_2
+  def pins
+    @rolls.inject(:+) || 0
+  end
+
+  def closed?
+    @rolls.length == 2 || pins == 10
   end
 
 end
