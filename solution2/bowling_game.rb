5f94bbec98cc0e57dc1f774f3b185681adf80feb
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-04 21:57:41 -0500

    simple solution 2

--- solution2/bowling_game.rb
+++ solution2/bowling_game.rb
@@ -1,3 +1,54 @@
+require_relative "test/mock"
+
 class BowlingGame
+
+  attr_accessor :frames
+
+  def initialize
+    @frames = []
+    @next = 0
+
+    10.times do
+      add_frame
+    end
+  end
+
+  def bowl(pins)
+    current_frame.rolls << pins
+  end
+
+  def score_game(fs)
+    i = 0
+    fs.inject(0) do |total, frame|
+      i += 1
+      score = frame_total(frame)
+      if frame[:strike]
+        if fs[i][:strike]
+          score += fs[i][:rolls][0] + fs[i+1][:rolls][0]
+        else
+          score += fs[i][:rolls][0] + fs[i][:rolls][1]
+        end
+      elsif frame[:spare]
+        score += fs[i][:rolls][0]
+      end
+      total + score
+    end
+  end
+
+
+  ### PRIVATE ###################################
+  private
+  def add_frame
+    frames << {
+      :strike   => false,
+      :spare    => false,
+      :rolls    => [],
+      :complete => false
+    }
+  end
+
+  def frame_total(frame)
+    frame[:rolls].inject(:+)
+  end
 
 end
--- solution2/test/test.rb
+++ solution2/test/test.rb
@@ -1,16 +1,45 @@
 require "test/unit"
 require_relative "../bowling_game"
+require_relative "mock"
 
 class TestBowlingGame < Test::Unit::TestCase
 
-  attr_accessor :game
+  attr_accessor :game, :frameset
 
   def setup
     @game = BowlingGame.new
+    @frameset = FrameSet.frameset
   end
 
   def test_game_exists
     assert(game)
+  end
+
+  def test_game_has_ten_frames
+    # frame count 10
+    assert(game.frames.length == 10)
+  end
+
+  def test_game_can_score_complete_frameset
+    score = game.score_game(frameset)
+    assert(score == 30)
+  end
+
+  def test_game_scores_open_frames
+    # scores open frame
+  end
+
+  def test_game_scores_spares
+    # throws if subs aren't available
+    # scores spare if subs are available
+  end
+
+  def test_game_scores_strikes
+    # throws if subs aren't available
+    # scores spare if subs are available
+  end
+
+  def test_game_can_have_extra_rolls
   end
 
   def teardown
