f58c575770195eb6eddaf9f1bcedd62d5a206fc0
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-08 10:44:41 -0500

    :white_checkmark: update test.rb, mock.rb

--- solution3/test/test.rb
+++ solution3/test/test.rb
@@ -13,24 +13,28 @@ class TestBowling < Test::Unit::TestCase
   def test_game_scores_all_strikes
     fs = FrameSet::all_strikes
     score = game.score_game(fs)
+    p score
     assert(score == 300)
   end
 
   def test_game_scores_all_spares
     fs = FrameSet::all_spares
     score = game.score_game(fs)
+    p score
     assert(score == 150)
   end
 
   def test_game_scores_all_open
     fs = FrameSet::all_open
     score = game.score_game(fs)
+    p score
     assert(score == 20)
   end
 
   def test_game_scores_mixed_frames
     fs = FrameSet::mixed_frames
     score = game.score_game(fs)
+    p score
     assert(score == 139)
   end
 
