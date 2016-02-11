692ac1771c3bfb5bb87d102bca11a67f0ab9ed98
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-11 12:51:49 -0500

    add solution5: scoring no longer changes data stored on game instance

---
+++ solution5/bowling_game.rb
@@ -0,0 +1,42 @@
+####################################
+# Solution inspired by Yock & Cat
+####################################
+
+class BowlingGame
+
+  attr_accessor :rolls
+
+  def initialize
+    @rolls = []
+  end
+
+  def bowl(pins)
+    rolls << pins
+  end
+
+  def score(rollz)
+    points = []
+    cur_roll = 0
+    10.times do
+      points << rollz.fetch(cur_roll, 0)
+      cur_roll += 1
+      if points.last == 10
+        points << rollz.fetch(cur_roll, 0) << rollz.fetch(cur_roll+1, 0)
+        next
+      end
+
+      if points.last + rollz.fetch(cur_roll, 0) == 10
+        points << rollz.fetch(cur_roll+1, 0)
+      end
+      points << rollz.fetch(cur_roll, 0)
+      cur_roll += 1
+    end
+    points.reduce(:+)
+  end
+
+  private
+  def shift(arr)
+    (arr.shift || 0)
+  end
+
+end
