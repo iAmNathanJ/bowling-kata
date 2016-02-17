1af762877ea53eb301b9762d8896e21e28b7d801
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-17 14:36:44 -0500

    update bowling_driver.rb: using Player class

--- solution5/bowling_driver.rb
+++ solution5/bowling_driver.rb
@@ -1,4 +1,5 @@
 require_relative "bowling_game"
+require_relative "player"
 
 class Console
   def write(msg); print msg; end
@@ -15,15 +16,8 @@ class BowlingGameDriver
   end
 
   def add_player(name)
-    status[:players] << {
-      :name => name,
-      :game => BowlingGame.new
-    }
+    status[:players] << Player.new(name, BowlingGame.new)
     yield(status[:players].last)
-  end
-
-  def player_to_s(player)
-    "#{player[:name][:first][0]}. #{player[:name][:last]}"
   end
 
   def add_players
@@ -41,7 +35,7 @@ class BowlingGameDriver
       }
 
       # Add player
-      add_player(player_name) { |player| @io.write "\n#{player_to_s(player)} Added!\n\n" }
+      add_player(player_name) { |player| @io.write "\n#{player.to_s} Added!\n\n" }
 
       # Add another player?
       again = @io.request("Add another? (y/n) ").downcase
@@ -56,8 +50,8 @@ class BowlingGameDriver
     status[:players].each do |player|
 
       # Get first roll
-      roll_1 = @io.request("#{player_to_s(player)}: How many pins would you like to knock down? ").to_i
-      player[:game].bowl(roll_1)
+      roll_1 = @io.request("#{player.to_s}: How many pins would you like to knock down? ").to_i
+      player.game.bowl(roll_1)
 
       # If strike, next player is up
       if roll_1 == 10
@@ -68,7 +62,7 @@ class BowlingGameDriver
       else
         @io.write "\nYou have #{10 - roll_1} pins left.\n"
         roll_2 = @io.request("How many pins would you like to knock down? ").to_i
-        player[:game].bowl(roll_2)
+        player.game.bowl(roll_2)
 
         if(roll_1 + roll_2 == 10)
           @io.write "\nSPARE!\n\n"
@@ -83,7 +77,7 @@ class BowlingGameDriver
   def play
     add_players do |players|
       player_list = []
-      players.each { |player| player_list << player_to_s(player) }
+      players.each { |player| player_list << player.to_s }
       @io.write "\nPlayers added: #{player_list.join(', ')}\n\n"
     end
 
@@ -91,7 +85,7 @@ class BowlingGameDriver
       play_frame do |stats|
         @io.write "=================\n"
         @io.write "Current Scores\n"
-        stats[:players].each { |player| @io.write "#{player_to_s(player)}: #{player[:game].score}\n" }
+        stats[:players].each { |player| @io.write "#{player.to_s}: #{player.game.score}\n" }
         @io.write "=================\n"
       end
     end while(!all_done?)
@@ -101,7 +95,7 @@ class BowlingGameDriver
   def all_done?
     done = true
     status[:players].each do |player|
-      if !player[:game].complete?
+      if !player.game.complete?
         done = false
         break
       end
@@ -111,7 +105,7 @@ class BowlingGameDriver
 
   def winner
     status[:players].reduce do |winner, player|
-      winner[:game].score > player[:game].score ? winner[:game].score : player[:game].score
+      winner.game.score > player.game.score ? winner.game.score : player.game.score
     end
   end
 
