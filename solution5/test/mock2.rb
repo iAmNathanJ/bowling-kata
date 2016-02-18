22a4929ddd930e1d2f32bf3c68542901b458574d
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-17 19:31:19 -0500

    update mock2.rb

--- solution5/test/mock2.rb
+++ solution5/test/mock2.rb
@@ -5,40 +5,28 @@ class MockSR
   def initialize(send_method_name, request_method_name, *requests)
     @send = send_method_name
     @request = request_method_name
-    @requests = requests
-    @requests.map do |req|
+    @requests = requests.map do |req|
       req[:request] = regexify(req[:request])
       req[:index] = 0
+      req
     end
   end
 
-  define_method(:@send) do |msg|
-    @latest_message = msg;
+  define_method(@send) do |msg|
+    received = msg;
   end
-  # def write(msg); @latest_message = msg; end
 
-  define_method(:@request) do |msg|
+  define_method(@request) do |msg|
     received = msg
     @requests.each do |req|
-      if msg.match(req[:question])
-        sent = req[:answers][q[:index]]
+      if msg.match(req[:request])
+        @sent = req[:answers][req[:index]]
         req[:index] += 1
         break
       end
     end
     sent
   end
-  # def request(msg)
-  #   received = msg
-  #   @requests.each do |req|
-  #     if msg.match(req[:question])
-  #       sent = req[:answers][q[:index]]
-  #       req[:index] += 1
-  #       break
-  #     end
-  #   end
-  #   sent
-  # end
 
   def reset
     @requests.each { |q| q[:index] = 0 }
@@ -53,20 +41,18 @@ end
 
 mock = MockSR.new("send", "request",
   {
-    question: regexify("Add a player (name) "),
-    answers: ["Suzy", "Nate", "Karl"],
-    index: 0
+    request: "Add a player (name) ",
+    answers: ["Suzy", "Nate", "Karl"]
   },
   {
-    question: regexify("Add another? (y/n) "),
-    answers: ["y", "y", "n"],
-    index: 0
+    request: "Add another? (y/n) ",
+    answers: ["y", "y", "n"]
   },
   {
-    question: regexify("How many pins would you like to knock down? "),
-    answers: [10, 5, 5, 1],
-    index: 0
+    request: "How many pins would you like to knock down? ",
+    answers: [10, 5, 5, 1]
   }
 )
 
-p mock
+mock.make_request("request")
+p mock.request("Add a player (name) ")
