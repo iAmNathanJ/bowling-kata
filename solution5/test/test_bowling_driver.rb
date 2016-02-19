618eda1544c82669fb93c6de01deebe331c923dd
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-19 09:55:31 -0500

    :white_check_mark: Update tests: move mock data setup into test methods to improve comprehension

--- solution5/test/test_bowling_driver.rb
+++ solution5/test/test_bowling_driver.rb
@@ -9,24 +9,18 @@ class TestBowlingDriver < Test::Unit::TestCase
   def setup
     @mock_io = MockIO.new
 
-    @mock_io.add_requests(
-      {
-        request: "(first name)",
-        response: ["Suzy", "Nate", "Nathan"]
-      },
-      {
-        request: "(last name)",
-        response: ["Riska", "Jacobs", "Jr."]
-      },
-      {
-        request: "Add another?",
-        response: ["y", "y", "n"]
-      },
-      {
-        request: "How many pins would you like to knock down?",
-        response: [10, 5, 5, 1]
-      }
-    )
+    @mock_io.add_requests({
+      request: "(first name)",
+      response: ["Suzy", "Nate", "Nathan"]
+    },
+    {
+      request: "(last name)",
+      response: ["Riska", "Jacobs", "Jr."]
+    },
+    {
+      request: "Add another?",
+      response: ["y", "y", "n"]
+    })
 
     @driver = BowlingGameDriver.new(@mock_io)
   end
@@ -50,36 +44,43 @@ class TestBowlingDriver < Test::Unit::TestCase
       assert_equal(3, players.length)
     end
 
-    @mock_io.add_response("(first name)", "Ryan", "Rob", "Kasey")
-    @mock_io.add_response("(last name)", "Cromwell", "Tarr", "Bon")
     @mock_io.reset_response_queue("Add another?")
+    @mock_io.add_response("(first name)", "Ryan", "Rob", "Bryan")
+    @mock_io.add_response("(last name)", "Cromwell", "Tarr", "Braun")
 
     driver.add_players do |players|
       assert_equal(6, players.length)
       assert_equal("R. Cromwell", players[3].to_s)
       assert_equal("R. Tarr", players[4].to_s)
-      assert_equal("K. Bon", players[5].to_s)
-    end
-
-    @mock_io.reset_response_queue
-
-    driver.add_players do |players|
-      assert_equal(9, players.length)
-      assert_equal("S. Riska", players[6].to_s)
-      assert_equal("N. Jacobs", players[7].to_s)
-      assert_equal("N. Jr.", players[8].to_s)
+      assert_equal("B. Braun", players[5].to_s)
     end
   end
 
   def test_plays_frame
     driver.add_players { |players| }
+    assert_nothing_thrown do
+      driver.play_frame { |game_status| }
+    end
+  end
+
+  def test_playing_frame_updates_player_scores
+
+    @mock_io.add_requests({
+      request: "How many pins would you like to knock down?",
+      response: [10, 5, 5, 1, 1, 10, 5, 4, 1, 1]
+    })
+
+    driver.add_players { |players| }
+    # play first frame
+    driver.play_frame { |game_status| }
+    # play second frame and test scores
     driver.play_frame do |game_status|
       player_1 = game_status[:players][0]
       player_2 = game_status[:players][1]
       player_3 = game_status[:players][2]
-      assert_equal([10], player_1.game.rolls)
-      assert_equal([5, 5], player_2.game.rolls)
-      assert_equal([1, 0], player_3.game.rolls)
+      assert_equal(30, player_1.game.score)
+      assert_equal(24, player_2.game.score)
+      assert_equal(4, player_3.game.score)
     end
   end
 
